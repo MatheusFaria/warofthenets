6442249117ffdedf12f2c25fb95001ec0086727d
Lucas Moura <lucas.moura128@gmail.com>
2013-10-13 16:41:43 -0300

    Pause State implementado

---
+++ include/pausestate.h
@@ -0,0 +1,30 @@
+#ifndef PAUSE_STATE_H
+#define PAUSE_STATE_H
+
+#include "gameestate.h"
+#include "gameobject.h"
+#include <vector>
+
+
+class PauseState : public GameState{
+
+public:
+
+	virtual void update();
+	virtual void render();
+
+	virtual bool onEnter();
+	virtual bool onExit();
+
+	virtual std::string getStateId();
+
+private:
+
+	static void toMenu();
+	static void backToGame();
+
+	vector<GameObject*> pauseObjects;
+	static const std::string pauseId;
+};
+
+#endif
