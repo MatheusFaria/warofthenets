b0062bc9e9ce4da73e6d2441005ec5eb283632c1
Matheus de Sousa Faria <matheus.sousa.faria@gmail.com>
2013-10-18 09:38:08 -0300

    Recolocando arquivos excluidos da engine

---
+++ src/engine/log.cpp
@@ -0,0 +1,17 @@
+#include "log.h"
+#include "SDL2/SDL.h"
+
+void Log::warn(string msg)
+{
+	cout << msg << ": [" << SDL_GetError() << "]" << endl;	
+}
+
+void Log::error(string msg)
+{
+	cout << msg << ": [" << SDL_GetError() << "]" << endl;	
+}
+
+void Log::info(string msg)
+{
+	cout << msg << endl;	
+}
---
+++ src/engine/sdlgameobject.cpp
@@ -0,0 +1,52 @@
+#include "sdlgameobject.h"
+#include "texturemanager.h"
+#include "render.h"
+
+SDLGameObject::SDLGameObject(int _x, int _y, int _width, int _height, string _imageId):GameObject() 
+{
+	this->position = Vector2D(_x, _y);
+	width = _width;
+	height=_height;
+	imageId = _imageId;
+
+	currentRow = 0;
+	currentFrame  = 0;
+}
+
+void
+SDLGameObject::draw()
+{
+	TextureManager::Instance()->drawFrame(imageId, (Uint32)position.getX(),(Uint32)position.getY(),width,
+	 height, currentRow, currentFrame,Render::getInstance()->getRenderer(), 0 );
+}
+
+void
+SDLGameObject::clean()
+{
+
+}
+
+void
+SDLGameObject::update()
+{
+	
+}
+	
+void
+SDLGameObject::setWidth(int width)
+{
+	this->width = width;
+}
+
+void
+SDLGameObject::setHeight(int height)
+{
+	this->height = height;
+}
+
+void
+SDLGameObject::setPosition(int x, int y)
+{
+	this->position.setX(x);
+	this->position.setY(y);
+}
