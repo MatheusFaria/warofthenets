5a0fdc8b9d7dbce160be84e3385ce94c0a018c38
Lucas Kanashiro <lucas-kanashiro@hotmail.com>
2013-10-26 16:16:11 -0200

    Preparando main para receber ip e porta do servidor como argumentos

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include "game.h"
+#include <stdio.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+	if(argc == 1 || argc > 2)
+	{
+		printf("Deve-se passar 2 argumentos [IP e porta do servidor");
+		exit(1);
+	}
 
 	Game::Instance()->init();
 	Game::Instance()->run();
